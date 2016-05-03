2b760060f2f0eaafd7932b798350998304f3f95f
余钦 <yuqin@yuqindeimac.local>
2016-05-03 20:26:03 +0800

    Initial Commit

---
+++ YQAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQAlertView
+//
+//  Created by 余钦 on 16/5/3.
+//  Copyright © 2016年 cmbfae Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YQAlertView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YQAlertView
+//
+//  Created by 余钦 on 16/5/3.
+//  Copyright © 2016年 cmbfae Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
