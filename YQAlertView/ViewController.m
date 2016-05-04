c9a99929943e5e6748217e9c4e17a87a711842a3
余钦 <yuqin@yuqindeiMac.local>
2016-05-04 11:13:16 +0800

    自定义弹框

--- YQAlertView/ViewController.m
+++ YQAlertView/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "YQAlertView.h"
 
-@interface ViewController ()
+@interface ViewController ()<YQAlertViewDelegate>
 
 @end
 
@@ -24,4 +25,25 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)PopupAlertview:(id)sender {
+   YQAlertView *alertView = [[YQAlertView alloc] initWithTitle:@"退出登录" message:@"fafsafsafsa挥洒返回卡50元萨肌肤快速减肥款沙发哈萨克飞机库萨积分卡费啊放假阿警方撒发烧发烧就发烧fakfjkajfkasjfklshf" delegate:self buttonTitles:@"确定", @"取消", @"ok", @"cacel" ,nil];
+    [alertView Show];
+}
+
+
+- (void)alertView:(YQAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    NSLog(@"---------->>>>>>buttonIndex:%d", buttonIndex);
+    
+}
+
+
+- (IBAction)popupSystem:(id)sender {
+    UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"system" message:@"xxxxfdjkfjasfjklsjfasfsadfsfsafsafsafsfsfks" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDestructive handler:nil];
+    
+    [alertVC addAction:action];
+    
+    [self presentViewController:alertVC animated:YES completion:nil];
+}
 @end
